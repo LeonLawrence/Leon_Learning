afd454ceaf871c749c3746a467adb8ca2b9e867f
LeonLawrence <leonlawrence@outlook.com>
2022-04-19 22:36:19 +0100

    Shell Sort Theory & Selection Sort Practice

--- src/main/java/data_structures_and_algorithms/dsa_1_sort_algorithms/dsa_2_selection_sort/Main2.java
+++ src/main/java/data_structures_and_algorithms/dsa_1_sort_algorithms/dsa_2_selection_sort/Main2.java
@@ -1,6 +1,6 @@
 package data_structures_and_algorithms.dsa_1_sort_algorithms.dsa_2_selection_sort;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
 
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
@@ -16,7 +16,6 @@ public class Main {
             }
             swap(intArray, largest, lastUnsortedIndex);
         }
-
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
@@ -33,3 +32,4 @@ public class Main {
         array[j] = temp;
     }
 }
+
---
+++ src/main/java/data_structures_and_algorithms/dsa_1_sort_algorithms/dsa_4_shell_sort/Main.java
@@ -0,0 +1,4 @@
+package data_structures_and_algorithms.dsa_1_sort_algorithms.dsa_4_shell_sort;
+
+public class Main {
+}
