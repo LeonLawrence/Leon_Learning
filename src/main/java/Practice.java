b96b219d88ce448e478305ab587d1eec4aac8cb2
LeonLawrence <leonlawrence@outlook.com>
2022-04-20 21:41:11 +0100

    Shell Sort Implementation & Practice

---
+++ src/main/java/Practice.java
@@ -0,0 +1,34 @@
+public class Practice {
+    // Anything added to this class will be deleted.
+
+    public static void main(String[] args) {
+
+        int[] intArray = { 45,93,2,8,-15,-98,64,37,1,-1,23,-22 };
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastUnsortedIndex);
+        }
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- src/main/java/data_structures_and_algorithms/dsa_1_sort_algorithms/dsa_4_shell_sort/Main.java
+++ src/main/java/data_structures_and_algorithms/dsa_1_sort_algorithms/dsa_4_shell_sort/Main.java
@@ -1,4 +1,30 @@
 package data_structures_and_algorithms.dsa_1_sort_algorithms.dsa_4_shell_sort;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[i] = intArray[j - gap];
+                    j -= gap;
+                }
+
+                intArray[j] = newElement;
+
+            }
+
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
 }
