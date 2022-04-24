3a3cd505e4ca68d29ce004025b59806d92e22b55
LeonLawrence <leonlawrence@outlook.com>
2022-04-24 22:13:38 +0100

    Array implementation using the JDK

---
+++ src/main/java/data_structures_and_algorithms/dsa_1_sort_algorithms/dsa_10_sorting_arrays_using_the_JDK/Main.java
@@ -0,0 +1,18 @@
+package data_structures_and_algorithms.dsa_1_sort_algorithms.dsa_10_sorting_arrays_using_the_JDK;
+
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+//        Arrays.sort(intArray);
+        Arrays.parallelSort(intArray);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+
+    }
+}
