5ca10a79aa5c2ca1780cf758a4af91fde0941deb
LeonLawrence <leonlawrence@outlook.com>
2022-04-26 22:15:50 +0100

    Array List Employee Class Implementation

--- src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_2_array_lists/Main.java
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_2_array_lists/Main.java
@@ -1,7 +1,11 @@
 package data_structures_and_algorithms.dsa_2_lists.dsa_2_array_lists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        
+        List<Employee> employeeList = new ArrayList<>();
     }
 }
