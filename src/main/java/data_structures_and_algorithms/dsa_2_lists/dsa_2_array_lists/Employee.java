b03fa1d43eca54b189cc1462f2c7dcdc43e3e841
LeonLawrence <leonlawrence@outlook.com>
2022-04-27 20:07:26 +0100

    Array List Update
    equal and hashcode contract implemented. Also explored ArrayList JDK

--- src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_2_array_lists/Employee.java
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_2_array_lists/Employee.java
@@ -1,5 +1,7 @@
 package data_structures_and_algorithms.dsa_2_lists.dsa_2_array_lists;
 
+import java.util.Objects;
+
 public class Employee {
 
     private String firstName;
@@ -34,6 +36,19 @@ public class Employee {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && firstName.equals(employee.firstName) && lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, id);
     }
 
     @Override
--- src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_2_array_lists/Main.java
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_2_array_lists/Main.java
@@ -13,13 +13,34 @@ public class Main {
         employeeList.add(new Employee("Mike", "Wilson", 3245));
 
 
-        employeeList.forEach(employee -> System.out.println(employee));
+//        employeeList.forEach(employee -> System.out.println(employee));
+//
+//        System.out.println(employeeList.get(1));
+//
+//        System.out.println(employeeList.isEmpty());
+//
 
-        System.out.println(employeeList.get(1));
-
-        System.out.println(employeeList.isEmpty());
-        System.out.println("--------------------------------------------------------------------");
         employeeList.set(1, new Employee("John", "Adams", 4568));
         employeeList.forEach(employee -> System.out.println(employee));
+
+
+        System.out.println(employeeList.size());
+
+        employeeList.add(3, new Employee("John", "Doe", 4567));
+        employeeList.forEach(employee -> System.out.println(employee));
+
+        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+        System.out.println("-----------------------------------------");
+//        for (Employee employee : employeeArray) {
+//            System.out.println(employee);
+
+        System.out.println(employeeList.contains(new Employee("Mary", "Smith", 22)));
+        System.out.println(employeeList.indexOf(new Employee("John", "Doe", 4567)));
+
+        employeeList.remove(2);
+
+        System.out.println("-------------------------------------------");
+        employeeList.forEach(employee -> System.out.println(employee));
+
     }
 }
