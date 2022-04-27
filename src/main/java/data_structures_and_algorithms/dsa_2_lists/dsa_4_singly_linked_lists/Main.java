988f5e01978fb961c2e69a5e6c466ff844e5672e
LeonLawrence <leonlawrence@outlook.com>
2022-04-27 22:09:57 +0100

    Learning Vectors

---
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_4_singly_linked_lists/Main.java
@@ -0,0 +1,4 @@
+package data_structures_and_algorithms.dsa_2_lists.dsa_4_singly_linked_lists;
+
+public class Main {
+}
---
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_5_doubly_linked_lists/Main.java
@@ -0,0 +1,4 @@
+package data_structures_and_algorithms.dsa_2_lists.dsa_5_doubly_linked_lists;
+
+public class Main {
+}
