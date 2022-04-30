79fdbe1116b96acee277d099b5f5a1dfc9e20278
LeonLawrence <leonlawrence@outlook.com>
2022-04-30 21:44:29 +0100

    Doubly LinkedList Implementation Update
    Feature: adding to the front and end of LinkedList & removing from front and end of LinkedList

--- src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_5_doubly_linked_lists/Main.java
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_5_doubly_linked_lists/Main.java
@@ -8,20 +8,30 @@ public class Main {
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
 
-        EmployeeLinkedList list = new EmployeeLinkedList();
-
-        System.out.println(list.isEmpty());
+        EmployeeDoublyLinkedList list = new EmployeeDoublyLinkedList();
 
         list.addToFront(janeJones);
         list.addToFront(johnDoe);
         list.addToFront(marySmith);
         list.addToFront(mikeWilson);
 
-        System.out.println(list.getSize());
         list.printList();
+        System.out.println(list.getSize());
+
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        list.addToEnd(billEnd);
+
+        list.printList();
+        System.out.println(list.getSize());
 
         list.removeFromFront();
-        System.out.println(list.getSize());
         list.printList();
+        System.out.println(list.getSize());
+
+        list.removeFromEnd();
+        list.printList();
+        System.out.println(list.getSize());
+
     }
 }
