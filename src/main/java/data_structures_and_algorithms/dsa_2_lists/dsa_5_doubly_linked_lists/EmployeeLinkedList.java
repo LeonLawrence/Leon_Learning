b93cebbf6f2ccfc5af8465fe1103412320bbcaeb
LeonLawrence <leonlawrence@outlook.com>
2022-04-30 21:11:43 +0100

    Doubly Linked List Implementation

---
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_5_doubly_linked_lists/EmployeeLinkedList.java
@@ -0,0 +1,50 @@
+package data_structures_and_algorithms.dsa_2_lists.dsa_5_doubly_linked_lists;
+
+public class EmployeeLinkedList {
+
+    private EmployeeNode head;
+    private EmployeeNode tail;
+    private int size;
+
+    public void addToFront(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node;
+        size++;
+    }
+
+    public void addToBack() {
+
+    }
+
+    public EmployeeNode removeFromFront() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        EmployeeNode removedNode = head;
+        head = head.getNext();
+        size--;
+        removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void printList() {
+        EmployeeNode current = head;
+        System.out.print("HEAD -> ");
+        while (current != null) {
+            System.out.print(current);
+            System.out.print(" -> ");
+            current = current.getNext();
+        }
+        System.out.println("null");
+    }
+}
--- src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_5_doubly_linked_lists/Main.java
+++ src/main/java/data_structures_and_algorithms/dsa_2_lists/dsa_5_doubly_linked_lists/Main.java
@@ -1,4 +1,27 @@
 package data_structures_and_algorithms.dsa_2_lists.dsa_5_doubly_linked_lists;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+
+        System.out.println(list.isEmpty());
+
+        list.addToFront(janeJones);
+        list.addToFront(johnDoe);
+        list.addToFront(marySmith);
+        list.addToFront(mikeWilson);
+
+        System.out.println(list.getSize());
+        list.printList();
+
+        list.removeFromFront();
+        System.out.println(list.getSize());
+        list.printList();
+    }
 }
