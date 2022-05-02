0b2f349bdddbd702a98d6d079ee5115b709d23b2
LeonLawrence <leonlawrence@outlook.com>
2022-05-02 21:15:30 +0100

    new branch for stacks & testing stacks using arrays

---
+++ src/main/java/data_structures_and_algorithms/dsa_3_stacks/dsa_1_stacks_using_arrays/ArrayStack.java
@@ -0,0 +1,17 @@
+package data_structures_and_algorithms.dsa_3_stacks.dsa_1_stacks_using_arrays;
+
+public class ArrayStack {
+
+    private Employee[] stack;
+    private int top;
+
+    public ArrayStack(int capacity) {
+        stack = new Employee[capacity];
+    }
+
+    public void push(Employee employee) {
+        if (top == stack.length) {
+            
+        }
+    }
+}
