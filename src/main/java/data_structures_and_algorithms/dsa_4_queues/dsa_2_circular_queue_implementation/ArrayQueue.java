44e3d426f7622f84e3a4fc5d2ac9530439c92d84
LeonLawrence <leonlawrence@outlook.com>
2022-05-10 22:47:06 +0100

    bug fix for queue capacity

---
+++ src/main/java/data_structures_and_algorithms/dsa_4_queues/dsa_2_circular_queue_implementation/ArrayQueue.java
@@ -0,0 +1,73 @@
+package data_structures_and_algorithms.dsa_4_queues.dsa_2_circular_queue_implementation;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue {
+
+    private Employee[] queue;
+    private int front;
+    private int back;
+
+    public ArrayQueue(int capacity) {
+        queue = new Employee[capacity];
+    }
+
+    public void add(Employee employee) {
+        if (back == queue.length) {
+            Employee[] newArray = new Employee[2 * queue.length];
+            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            queue = newArray;
+        }
+
+        // 0 - jane - front
+        // 1 - john - front
+        // 2 -         -back
+        // 3
+        // 4
+
+
+
+
+
+        queue[back] = employee;
+        if (back < queue.length - 1) {
+        back++;
+        } else {
+            back = 0;
+        }
+    }
+
+    public Employee remove() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+
+        Employee employee = queue[front];
+        queue[front] = null;
+        front++;
+        if (size() == 0) {
+            front = 0;
+            back = 0;
+        }
+
+        return employee;
+    }
+
+    public Employee peek() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
+
+        return queue[front];
+    }
+
+    public int size() {
+        return back - front;
+    }
+
+    public void printQueue() {
+        for (int i = front; i < back; i++) {
+            System.out.println(queue[i]);
+        }
+    }
+}
