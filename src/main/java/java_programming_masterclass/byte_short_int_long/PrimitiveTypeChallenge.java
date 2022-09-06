219cfa65508024d467d6169104ee1a5b6e8ee6ba
LeonLawrence <leonlawrence@outlook.com>
2022-09-06 22:35:31 +0100

    updated primitive type challenge

--- src/main/java/java_programming_masterclass/byte_short_int_long/PrimitiveTypeChallenge.java
+++ src/main/java/java_programming_masterclass/byte_short_int_long/PrimitiveTypeChallenge.java
@@ -14,15 +14,24 @@ public class PrimitiveTypeChallenge {
         the byte plus the short plus the integer values.
          */
 
-        byte myByte = 26;
-        short myShort = 123;
-        int myInt = 157;
+        byte myByte = 10;
+        short myShort = 20;
+        int myInt = 50;
         long myLong = 50000L;
 
         int sum = myByte + myShort + myInt;
-
         int newSum = (int) myLong + 10 * sum;
-
         System.out.println(newSum);
+
+        /*
+        Course Solution:
+        long longTotal = 50000L + 10L * (myByte + myShort + myInt);
+        System.out.println(longTotal);
+         */
+
+        /*
+        Non Challenge Example:
+        short shortTotal = (short) (1000 + 10 * (myByte + myShort + myInt));
+         */
     }
 }
