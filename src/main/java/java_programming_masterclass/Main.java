e4334bf52dca1540c5379744c23455cf3158ff2d
LeonLawrence <leonlawrence@outlook.com>
2022-09-06 15:24:11 +0100

    starting out with expressions

--- src/main/java/java_programming_masterclass/Main.java
+++ src/main/java/java_programming_masterclass/Main.java
@@ -2,11 +2,23 @@ package java_programming_masterclass;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
         System.out.println("My name is Leon");
 
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+
+        // Challenge
+        // Create a new variable called myLastOne.
+        // We want the value to be 1000 less than the current value of myTotal - the data type is an int
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
