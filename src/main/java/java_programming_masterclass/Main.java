248ddf8e94a266243b31a200c1c5fd5ab8eb7b6c
LeonLawrence <leonlawrence@outlook.com>
2022-09-06 15:08:15 +0100

    variables

--- src/main/java/java_programming_masterclass/Main.java
+++ src/main/java/java_programming_masterclass/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("My name is Leon");
+
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
     }
 }
