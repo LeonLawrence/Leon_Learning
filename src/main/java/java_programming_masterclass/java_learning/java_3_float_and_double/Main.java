c79e499111dad2832ac715b7dcae688174163f37
LeonLawrence <leonlawrence@outlook.com>
2022-09-07 11:20:30 +0100

    Float Coding Challenge

--- src/main/java/java_programming_masterclass/java_learning/java_3_float_and_double/Main.java
+++ src/main/java/java_programming_masterclass/java_learning/java_3_float_and_double/Main.java
@@ -1,4 +1,22 @@
 package java_programming_masterclass.java_learning.java_3_float_and_double;
 
 public class Main {
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMaxFloatValue);
+        System.out.println("Float minimum value = " + myMinFloatValue);
+
+        System.out.println("----------------------------------------------------------");
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("double minimum value = " + myMaxDoubleValue);
+        System.out.println("double minimum value = " + myMinDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = (float) 5.25;
+        double myDoubleValue = 5.25;
+    }
 }
