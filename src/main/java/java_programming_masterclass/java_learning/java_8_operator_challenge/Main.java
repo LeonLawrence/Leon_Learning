c0bb8c713f6c445827892923c79d76f7c0c48e3b
LeonLawrence <leonlawrence@outlook.com>
2022-09-07 22:12:27 +0100

    Operator Challenge

---
+++ src/main/java/java_programming_masterclass/java_learning/java_8_operator_challenge/Main.java
@@ -0,0 +1,22 @@
+package java_programming_masterclass.java_learning.java_8_operator_challenge;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double myFirstValue = 20.00;
+        double mySecondValue = 80.00;
+
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("MyValueTotal = " + myValuesTotal);
+        System.out.println("theRemainder = " + theRemainder);
+
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + theRemainder);
+
+        if (!isNoRemainder) {
+            System.out.println("Got Some Remainder");
+        }
+
+    }
+}
