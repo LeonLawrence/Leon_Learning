6d186f25f2906d78b46895cd055b390fa79fdc03
LeonLawrence <leonlawrence@outlook.com>
2022-09-09 16:03:16 +0100

    logic challenge logic bug fix

--- src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_4_methods/Main2.java
+++ src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_4_methods/Main2.java
@@ -7,6 +7,10 @@ public class Main2 {
         displayHighScorePosition("Tony", calculateHighScorePosition(900));
         displayHighScorePosition("Binh", calculateHighScorePosition(400));
         displayHighScorePosition("Stephen", calculateHighScorePosition(50));
+
+        int highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Maik", highScorePosition);
+
     }
 
     public static void displayHighScorePosition(String playerName, int position) {
@@ -15,11 +19,11 @@ public class Main2 {
 
 
     static int calculateHighScorePosition(int playerScore) {
-        if (playerScore > 1000) {
+        if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore > 500 && playerScore < 1000) {
+        } else if (playerScore >= 500 && playerScore < 1000) {
             return 2;
-        } else if (playerScore > 100 && playerScore < 500) {
+        } else if (playerScore >= 100 && playerScore < 500) {
             return 3;
         } else {
             return 4;
