c9d4d849a3914a2be55d262c19efc1f523016578
LeonLawrence <leonlawrence@outlook.com>
2022-09-29 11:36:35 +0100

    OOP Revision - Classes and Sum Calculator Exercise

---
+++ src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_8_OOP/java_3_person_exercise/Main.java
@@ -0,0 +1,4 @@
+package java_programming_masterclass.java_learning.learning_2_java_tutorial.java_8_OOP.java_3_person_exercise;
+
+public class Main {
+}
---
+++ src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_8_OOP/java_4_constructors/Main.java
@@ -0,0 +1,4 @@
+package java_programming_masterclass.java_learning.learning_2_java_tutorial.java_8_OOP.java_4_constructors;
+
+public class Main {
+}
