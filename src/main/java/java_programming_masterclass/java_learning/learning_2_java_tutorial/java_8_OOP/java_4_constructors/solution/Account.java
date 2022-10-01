2f70205f683616b3eba55d1b7f4f7bf00a7df68c
LeonLawrence <leonlawrence@outlook.com>
2022-10-01 20:48:25 +0100

    Constructor Exercise

--- src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_8_OOP/java_4_constructors/solution/Account.java
+++ src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_8_OOP/java_4_constructors/solution/Account.java
@@ -13,11 +13,16 @@ public class Account {
         System.out.println("Empty constructor called");
     }
     public Account(String number, double balance, String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        System.out.println("Account constructor with parameters called");
         this.number = number;
         this.balance = balance;
         this.customerName = customerName;
         this.customerEmailAddress = customerEmailAddress;
         this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public Account(String customerName, String customerEmailAddress, String customerPhoneNumber) {
+        this("99999", 100.55, customerName, customerEmailAddress, customerPhoneNumber);
     }
 
     public void deposit(double depositAmount) {
--- src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_8_OOP/java_4_constructors/solution/Main.java
+++ src/main/java/java_programming_masterclass/java_learning/learning_2_java_tutorial/java_8_OOP/java_4_constructors/solution/Main.java
@@ -2,18 +2,39 @@ package java_programming_masterclass.java_learning.learning_2_java_tutorial.java
 
 public class Main {
     public static void main(String[] args) {
-        Account leonsAccount = new Account("12345", 100,"Leon Lawrence", "Leon@email.com","07111111111");
-        Account leonsAccounttest = new Account();
-        leonsAccount.withdrawal(100);
+//        Account leonsAccount = new Account("12345", 100,"Leon Lawrence", "Leon@email.com","07111111111");
+//        Account leonsAccounttest = new Account();
+//        leonsAccount.withdrawal(100);
+//
+//        leonsAccount.deposit(50);
+//        leonsAccount.withdrawal(100);
+//
+//        leonsAccount.deposit(51);
+//        leonsAccount.withdrawal(100);
+//        System.out.println("-----------");
+//        System.out.println(leonsAccounttest.getBalance());
+//        System.out.println(leonsAccounttest.getCustomerName());
+//
+//        System.out.println("-----------");
+//        Account testAccount = new Account("Test", "test@gmail.com", "12345");
+//        System.out.println(testAccount.getCustomerName());
+//
+//        System.out.println("-----------");
+        VipCustomer vipCustomer = new VipCustomer();
+        System.out.println(vipCustomer.getName());
+        System.out.println(vipCustomer.getCreditLimit());
+        System.out.println(vipCustomer.getEmailAddress());
 
-        leonsAccount.deposit(50);
-        leonsAccount.withdrawal(100);
-
-        leonsAccount.deposit(51);
-        leonsAccount.withdrawal(100);
         System.out.println("-----------");
-        System.out.println(leonsAccounttest.getBalance());
-        System.out.println(leonsAccounttest.getCustomerName());
+
+        VipCustomer person1 = new VipCustomer();
+        System.out.println(person1.getName());
+
+        VipCustomer person2 = new VipCustomer("bob", 25000.00, "bob@email.com");
+        System.out.println(person2.getName());
+
+        VipCustomer person3 = new VipCustomer("tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
 
     }
 }
