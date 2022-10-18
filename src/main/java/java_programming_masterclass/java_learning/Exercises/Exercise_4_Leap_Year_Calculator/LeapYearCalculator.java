01b27e88f21bcd629d98dfc79f799cb442333f86
LeonLawrence <leonlawrence@outlook.com>
2022-10-19 00:00:00 +0100

    Java Exercise Leap Year Calculator

---
+++ src/main/java/java_programming_masterclass/java_learning/Exercises/Exercise_4_Leap_Year_Calculator/LeapYearCalculator.java
@@ -0,0 +1,25 @@
+package java_programming_masterclass.java_learning.Exercises.Exercise_4_Leap_Year_Calculator;
+
+public class LeapYearCalculator {
+
+    // Purpose: Is year a leap Year
+
+    private boolean isLeapYear(int year) {
+        // Invalid options
+        if (year >= 1 && year <= 9999) {
+            // Purpose
+            if (year % 4 == 0) {
+                if (year % 100 == 0 || year % 400 == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void printCalculation(int year) {
+        boolean leapYear = isLeapYear(year);
+        System.out.println(leapYear);
+    }
+
+}
---
+++ src/main/java/java_programming_masterclass/java_learning/Exercises/Exercise_4_Leap_Year_Calculator/Main.java
@@ -0,0 +1,12 @@
+package java_programming_masterclass.java_learning.Exercises.Exercise_4_Leap_Year_Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
+
+        leapYearCalculator.printCalculation(-1600);
+        leapYearCalculator.printCalculation(1600);
+        leapYearCalculator.printCalculation(2017);
+        leapYearCalculator.printCalculation(2000);
+    }
+}
