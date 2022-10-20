63b1821a89f66c0cabb64100f4abf29b0393fd7d
LeonLawrence <leonlawrence@outlook.com>
2022-10-20 17:22:34 +0100

    Java Exercise MinutesToYearAndDayCalculator

--- src/main/java/java_programming_masterclass/java_learning/Exercises/Exercise_8_Area_Calculator/Main.java
+++ src/main/java/java_programming_masterclass/java_learning/Exercises/Exercise_8_Area_Calculator/Main.java
@@ -8,9 +8,5 @@ public class Main {
         System.out.println(areaCalculator.area(-1));
         System.out.println(areaCalculator.area(5.0, 4.0));
         System.out.println(areaCalculator.area(-1.0, 4.0));
-        areaCalculator.area(5.0);
-        areaCalculator.area(-1);
-        areaCalculator.area(5.0, 4.0);
-        areaCalculator.area(-1.0, 4.0);
     }
 }
