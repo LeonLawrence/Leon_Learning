199933c216ed743e918942f054a4422b3bbced34
LeonLawrence <leonlawrence@outlook.com>
2022-10-20 18:48:37 +0100

    Java CatPlaying Exercise

---
+++ src/main/java/java_programming_masterclass/java_learning/Exercises/Exercise_11_Playing_Cat/Main.java
@@ -0,0 +1,14 @@
+package java_programming_masterclass.java_learning.Exercises.Exercise_11_Playing_Cat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PlayingCat playingCat = new PlayingCat();
+
+        System.out.println(playingCat.isCatPlaying(true, 10));
+        System.out.println(playingCat.isCatPlaying(false, 36));
+        System.out.println(playingCat.isCatPlaying(false, 35));
+
+
+    }
+}
