1c1954223308e426ca9562741971f17dba097540
LeonLawrence <leonlawrence@outlook.com>
2022-10-25 21:35:09 +0100

    Java Array Challenge Exercise

---
+++ src/main/java/java_programming_masterclass/java_learning/learning_3_arrays_inbuilt_list_autoboxing_and_unboxing/java_1_arrays/Challenge.java
@@ -0,0 +1,65 @@
+package java_programming_masterclass.java_learning.learning_3_arrays_inbuilt_list_autoboxing_and_unboxing.java_1_arrays;
+
+import java.util.Scanner;
+
+public class Challenge {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        int[] intArray = getIntegers(5);
+
+        int[] sorted = sortIntegers(intArray);
+
+        printArray(sorted);
+
+//        for (int i = 0; i < intArray.length; i++) {
+//            System.out.println(values[i]);
+//        }
+    }
+
+    public static int[] getIntegers(int capacity) {
+        int[] array = new int[capacity];
+        System.out.println("Enter " + capacity + " integer values:\r");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+
+        // element 0   50
+        // element 1   160
+        // element 2   40
+
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
---
+++ src/main/java/java_programming_masterclass/java_learning/learning_3_arrays_inbuilt_list_autoboxing_and_unboxing/java_1_arrays/Main.java
@@ -0,0 +1,24 @@
+package java_programming_masterclass.java_learning.learning_3_arrays_inbuilt_list_autoboxing_and_unboxing.java_1_arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] intArray = new int[10];
+
+        int[] intArray = new int[25]; //{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i * 10;
+        }
+
+//        for (int i = 0; i < intArray.length; i++) {
+//            System.out.println("Element " + i + ", value");
+//        }
+        printArray(intArray);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length;i++) {
+            System.out.println("Element " + i + ", value");
+        }
+    }
+}
