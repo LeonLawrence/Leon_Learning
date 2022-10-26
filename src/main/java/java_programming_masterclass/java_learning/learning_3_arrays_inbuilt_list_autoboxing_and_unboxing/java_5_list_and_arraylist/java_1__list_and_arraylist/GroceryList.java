32fc87f104905409cb77c063e0bd393c61caaf99
LeonLawrence <leonlawrence@outlook.com>
2022-10-26 23:22:52 +0100

    ArrayList Revision

---
+++ src/main/java/java_programming_masterclass/java_learning/learning_3_arrays_inbuilt_list_autoboxing_and_unboxing/java_5_list_and_arraylist/java_1__list_and_arraylist/GroceryList.java
@@ -0,0 +1,47 @@
+package java_programming_masterclass.java_learning.learning_3_arrays_inbuilt_list_autoboxing_and_unboxing.java_5_list_and_arraylist.java_1__list_and_arraylist;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+
+    private static ArrayList<String> groceryList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void addGroceryItem(String item) {
+
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + "items in your grocery list");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public static void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified");
+    }
+
+    public static void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        // Milk
+        // Bread
+    }
+
+    public String findItem(String searchItem) {
+//        boolean exist = groceryList.contains(searchItem);
+
+        int position = groceryList.indexOf(searchItem);
+
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+        return null;
+    }
+}
